d29d106aa4bbdac210ecf4489ff1167445b18056
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-01-29 21:14:28 +0100

    MVP

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const currentGuess = ref(['B', 'F']);
+const guesses = ref<{ letter: string, color: string }[][]>([]);
+const solution = ref('taart');
+
+window.addEventListener('keydown', function (e) {
+  if (currentGuess.value.length === 5 && e.code === 'Enter') {
+    guesses.value = [...guesses.value, currentGuess.value.map((letter, index) =>
+      ({ color: getColor(letter, index, solution.value), letter })
+    )];
+
+    console.log(guesses.value);
+
+    currentGuess.value = [];
+  }
+
+  if (currentGuess.value.length >= 5 || !e.code.startsWith('Key')) {
+    return;
+  }
+
+  currentGuess.value = [...currentGuess.value, e.key.toUpperCase()];
+});
+
+function getColor(key: string, index: number, solution: string) {
+  console.log(solution, index, key)
+  if (solution[index].toUpperCase() === key) {
+    return 'bg-green-600';
+  }
+
+  if (solution.toUpperCase().split('').includes(key)) {
+    return 'bg-orange-400';
+  }
+
+  return 'bg-gray-600';
+}
+</script>
+
+<template>
+  <div class="mx-auto font-bold">
+    <div class="flex" v-for="row in 6">
+    <!-- Past guesses -->
+      <div
+        v-if="guesses.length >= row"
+        class="h-12 w-12 border grid place-items-center"
+        :class="guesses[row - 1][letter - 1].color"
+        v-for="letter in 5"
+      >
+        <span
+          v-if="guesses[row - 1]"
+        >{{ guesses[row - 1][letter - 1].letter }}</span>
+      </div>
+      <div
+        v-else
+        class="h-12 w-12 border grid place-items-center"
+        v-for="letter in 5"
+      >
+        <span
+          v-if="row === guesses.length + 1 && currentGuess[letter - 1]"
+        >{{ currentGuess[letter - 1] }}</span>
+      </div>
+    </div>
+  </div>
+</template>
