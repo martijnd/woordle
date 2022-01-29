6d149c8af027b393b9274f05a0ba6cbd13e1e59b
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-01-29 21:55:12 +0100

    Add keyboard

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@ const container = ref<HTMLDivElement | null>(null);
 
 
 window.addEventListener('keydown', function (e) {
-  // Backspace
-  if (currentGuess.value.length > 0 && e.code === 'Backspace') {
+  handleLetterPress(e.code);
+});
+
+function handleLetterPress(code: string) {
+// Backspace
+  if (currentGuess.value.length > 0 && code === 'Backspace') {
     currentGuess.value = currentGuess.value.filter((_, idx) => idx !== currentGuess.value.length - 1);
   }
 
   // Enter
-  if (currentGuess.value.length === WORD_LENGTH && e.code === 'Enter') {
+  if (currentGuess.value.length === WORD_LENGTH && code === 'Enter') {
     guesses.value = [...guesses.value, currentGuess.value.map((letter, index) =>
       ({ color: getColor(letter, index, solution.value), letter })
     )];
@@ -25,12 +29,12 @@ window.addEventListener('keydown', function (e) {
     currentGuess.value = [];
   }
 
-  if (currentGuess.value.length >= WORD_LENGTH || !e.code.startsWith('Key')) {
+  if (currentGuess.value.length >= WORD_LENGTH || !code.startsWith('Key')) {
     return;
   }
 
-  currentGuess.value = [...currentGuess.value, e.key.toUpperCase()];
-});
+  currentGuess.value = [...currentGuess.value, code[3].toUpperCase()];
+}
 
 function getColor(key: string, index: number, solution: string) {
   if (solution[index].toUpperCase() === key) {
@@ -46,31 +50,74 @@ function getColor(key: string, index: number, solution: string) {
 </script>
 
 <template>
-  <div class="mx-auto font-bold" ref="container" @click="container?.focus()">
-    <div class="flex" v-for="row in AVAILABLE_GUESSES">
-      <!-- Past guesses -->
-      <div
-        v-if="guesses.length >= row"
-        class="h-12 w-12 border grid place-items-center"
-        :class="guesses[row - 1][letter - 1].color"
-        v-for="letter in WORD_LENGTH"
-      >
-        <input
-          class="bg-transparent w-3 outline-none cursor-default"
-          v-if="guesses[row - 1]"
-          :value="guesses[row - 1][letter - 1].letter"
-        />
+  <div class="relative">
+    <div
+      class="mx-auto min-h-screen max-w-sm font-bold flex flex-col justify-center items-center"
+      ref="container"
+      @click="container?.focus()"
+    >
+      <div class="flex" v-for="row in AVAILABLE_GUESSES">
+        <!-- Past guesses -->
+        <div
+          v-if="guesses.length >= row"
+          class="h-12 w-12 border grid place-items-center"
+          :class="guesses[row - 1][letter - 1].color"
+          v-for="letter in WORD_LENGTH"
+        >
+          <span
+            v-if="guesses[row - 1]"
+            v-text="guesses[row - 1][letter - 1].letter"
+          />
+        </div>
+        <div
+          v-else
+          class="h-12 w-12 border grid place-items-center"
+          v-for="letter in WORD_LENGTH"
+        >
+          <span
+            v-if="row === guesses.length + 1 && currentGuess[letter - 1]"
+            v-text="currentGuess[letter - 1]"
+          />
+        </div>
       </div>
-      <div
-        v-else
-        class="h-12 w-12 border grid place-items-center"
-        v-for="letter in WORD_LENGTH"
-      >
-        <input
-          class="bg-transparent w-3 outline-none cursor-default"
-          v-if="row === guesses.length + 1 && currentGuess[letter - 1]"
-          :value="currentGuess[letter - 1]"
-        />
+    </div>
+    <div class="absolute bottom-0 w-full text-center max-w-lg font-bold">
+      <div class="flex justify-between m-2">
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyQ')">Q</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyW')">W</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyE')">E</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyR')">R</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyT')">T</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyY')">Y</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyU')">U</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('Keyi')">i</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyO')">O</div>
+        <div class="px-4 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyP')">P</div>
+      </div>
+      <div class="flex justify-between m-2">
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyA')">A</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyS')">S</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyD')">D</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyF')">F</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyG')">G</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyH')">H</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyJ')">J</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyK')">K</div>
+        <div class="px-5 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyL')">L</div>
+      </div>
+      <div class="flex justify-between m-2">
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('Enter')">ENTER</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyZ')">Z</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyX')">X</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyC')">C</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyV')">V</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyB')">B</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyN')">N</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyM')">M</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('Keyi')">i</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyO')">O</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('KeyP')">P</div>
+        <div class="px-3 py-1 bg-gray-600 grid place-items-center cursor-pointer rounded-sm" @click="handleLetterPress('Backspace')">&lt;</div>
       </div>
     </div>
   </div>
